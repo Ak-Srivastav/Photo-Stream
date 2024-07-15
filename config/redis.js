5b3c585b23b9271acdc6c789eb682709cbc902e1
Ak-Srivastav <amit.edu@outlook.com>
2024-07-16 04:38:44 +0530

    adding

---
+++ config/redis.js
@@ -0,0 +1,42 @@
+import Redis from "ioredis";
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } from "./index.js";
+
+// for dev
+// let redis;
+// try {
+//   redis = new Redis({
+//     port: Number(REDIS_PORT), // Redis port
+//     host: REDIS_HOST, // Redis host
+//     username: "default", // needs Redis >= 6
+//     password: REDIS_PASSWORD,
+//     db: 0, // Defaults to 0
+//   });
+// } catch (err) {
+//   console.log("err", err);
+// }
+
+import createClient from 'ioredis';
+
+console.log("REDIS:", REDIS_HOST, REDIS_PORT, REDIS_PASSWORD)
+
+// const redis = new createClient({
+//   password: REDIS_PASSWORD,
+//   socket: {
+//     host: REDIS_HOST,
+//     port: parseInt(process.env.REDIS_PORT)
+//   }
+// });
+
+// const redis = new createClient({
+//   url: ":6379",  // works from Docker!
+// });
+
+const redis = new createClient({
+  // host: 'localhost',
+  host:'redis-ds',
+  port: 6379,
+  password: 'password'
+});
+
+
+export default redis;
