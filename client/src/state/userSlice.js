9a07849548c266131b481811f4e4f4e139509e77
Ak-Srivastav <amit.edu@outlook.com>
2024-07-29 20:05:09 +0530

    fixing google login

--- client/src/state/userSlice.js
+++ client/src/state/userSlice.js
@@ -33,27 +33,27 @@ export default userSlice.reducer;
 
 export const register =
   ({ values, resetForm, setSubmitting, navigate }) =>
-    () => {
-      const { firstName, lastName, email, password } = values;
-      instance
-        .post("/api/v1/auth/register", {
-          firstName: firstName.trim(),
-          lastName: lastName.trim(),
-          email: email.trim().toLowerCase(),
-          password: password.trim(),
-        })
+  () => {
+    const { firstName, lastName, email, password } = values;
+    instance
+      .post("/api/v1/auth/register", {
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+        email: email.trim().toLowerCase(),
+        password: password.trim(),
+      })
 
-        .then((data) => {
-          navigate("/signin");
-          resetForm();
-          toast.success(data.data.message);
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => setSubmitting(false));
-    };
+      .then((data) => {
+        navigate("/signin");
+        resetForm();
+        toast.success(data.data.message);
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => setSubmitting(false));
+  };
 
 export const login =
   ({
@@ -64,56 +64,56 @@ export const login =
     setEmailVerificationAlert,
     toggleBackdrop,
   }) =>
-    (dispatch) => {
-      const { email, password, isPersistent } = values;
+  (dispatch) => {
+    const { email, password, isPersistent } = values;
 
-      instance
-        .post(
-          "/api/v1/auth/login",
-          {
-            email: email.trim().toLowerCase(),
-            password: password.trim(),
-            isPersistent,
+    instance
+      .post(
+        "/api/v1/auth/login",
+        {
+          email: email.trim().toLowerCase(),
+          password: password.trim(),
+          isPersistent,
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        const popup = {
+          onSuccess: {
+            message: "Login successfull!",
+            type: "success",
           },
-          { withCredentials: true }
-        )
-        .then(() => {
-          const popup = {
-            onSuccess: {
-              message: "Login successfull!",
-              type: "success",
-            },
-            onError: {
-              message: "Something went wrong. Please try again..!",
-              type: "info",
-            },
-          };
-          const args = {
-            alert: setEmailVerificationAlert,
-            toggleBackdrop,
-            popup,
-            navigate,
-            to,
-          };
-          return dispatch(verifyUser(args));
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => setSubmitting(false));
-    };
+          onError: {
+            message: "Something went wrong. Please try again..!",
+            type: "info",
+          },
+        };
+        const args = {
+          alert: setEmailVerificationAlert,
+          toggleBackdrop,
+          popup,
+          navigate,
+          to,
+        };
+        return dispatch(verifyUser(args));
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => setSubmitting(false));
+  };
 
 export const logOut =
   ({ toggleBackdrop, navigate, handleCloseUserMenu, popupMsg = true }) =>
-    (dispatch) => {
-      dispatch(setStatus(STATUS.LOADING));
-      privateInstance
-        .post("/api/v1/auth/logout")
-        .then(() => {
-          if (handleCloseUserMenu) handleCloseUserMenu();
-          const popup = popupMsg
-            ? {
+  (dispatch) => {
+    dispatch(setStatus(STATUS.LOADING));
+    privateInstance
+      .post("/api/v1/auth/logout")
+      .then(() => {
+        if (handleCloseUserMenu) handleCloseUserMenu();
+        const popup = popupMsg
+          ? {
               onSuccess: {
                 title: "Something went wrong. Please try again..!",
                 type: "info",
@@ -123,30 +123,36 @@ export const logOut =
                 type: "success",
               },
             }
-            : null;
+          : null;
 
-          const args = {
-            toggleBackdrop,
-            popup,
-            navigate,
-            to: "/signin",
-          };
-          return dispatch(verifyUser(args));
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => dispatch(setStatus(STATUS.IDLE)));
-    };
+        const args = {
+          toggleBackdrop,
+          popup,
+          navigate,
+          to: "/signin",
+        };
+        return dispatch(verifyUser(args));
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => dispatch(setStatus(STATUS.IDLE)));
+  };
 
 export const loginWithGoogle = () => () => {
+  console.log("logging with google");
   window.open(
-    `${MODE === "dev" ? SERVER_DEV_API : SERVER_PROD_API}/api/v1/auth/google`,
+    `https://photo-stream-uxkd.onrender.com/api/v1/auth/google`,
     "_self"
   );
+  // window.open(
+  //   `${MODE === "dev" ? SERVER_DEV_API : SERVER_PROD_API}/api/v1/auth/google`,
+  //   "_self"
+  // );
 };
 export const loginWithFacebook = () => () => {
+  console.log("logging with facebook\n", SERVER_DEV_API, SERVER_PROD_API);
   window.open(
     `${MODE === "dev" ? SERVER_DEV_API : SERVER_PROD_API}/api/v1/auth/facebook`,
     "_self"
@@ -155,41 +161,41 @@ export const loginWithFacebook = () => () => {
 
 export const verifyUser =
   ({ alert, toggleBackdrop, popup, navigate, to }) =>
-    (dispatch) => {
-      toggleBackdrop("Please wait while we verify your login status.");
-      privateInstance
-        .get("/api/v1/auth/me")
-        .then((data) => {
-          console.log("setting user");
-          const { user } = data?.data;
-          dispatch(setUser(user));
-          if (popup) {
-            const { message, type } = popup.onSuccess;
-            toast[type](message);
-          }
-          if (navigate && to) {
-            setTimeout(() => navigate(to, { replace: true }), 0);
-          }
-          if (alert && !user?.verified) {
-            alert(true);
-          }
-        })
-        .catch(async (err) => {
-          console.log("erasing user");
-          dispatch(clearUser());
-          dispatch(clearUserPosts());
-          if (popup) {
-            const { message, type } = popup.onError;
-            toast[type](message);
-          }
-          if (navigate && to) {
-            setTimeout(() => navigate(to, { replace: true }), 0);
-          }
-        })
-        .finally(() => {
-          toggleBackdrop();
-        });
-    };
+  (dispatch) => {
+    toggleBackdrop("Please wait while we verify your login status.");
+    privateInstance
+      .get("/api/v1/auth/me")
+      .then((data) => {
+        console.log("setting user");
+        const { user } = data?.data;
+        dispatch(setUser(user));
+        if (popup) {
+          const { message, type } = popup.onSuccess;
+          toast[type](message);
+        }
+        if (navigate && to) {
+          setTimeout(() => navigate(to, { replace: true }), 0);
+        }
+        if (alert && !user?.verified) {
+          alert(true);
+        }
+      })
+      .catch(async (err) => {
+        console.log("erasing user");
+        dispatch(clearUser());
+        dispatch(clearUserPosts());
+        if (popup) {
+          const { message, type } = popup.onError;
+          toast[type](message);
+        }
+        if (navigate && to) {
+          setTimeout(() => navigate(to, { replace: true }), 0);
+        }
+      })
+      .finally(() => {
+        toggleBackdrop();
+      });
+  };
 
 export const refreshToken = () => () => {
   return instance.get("/api/v1/auth/refreshtoken", { withCredentials: true });
@@ -238,42 +244,42 @@ export const removeUserAvatar = (handleClose) => (dispatch) => {
 
 export const changePassword =
   ({ values, setSubmitting, handleClose, navigate, toggleBackdrop }) =>
-    (dispatch) => {
-      const { oldPassword, newPassword } = values;
-      privateInstance
-        .patch("/api/v1/user/changepassword", {
-          oldPassword: oldPassword.trim(),
-          newPassword: newPassword.trim(),
-        })
-        .then((data) => {
-          handleClose();
-          dispatch(logOut({ toggleBackdrop, navigate, popupMsg: false }));
-          toast.success(data.data?.message);
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => setSubmitting(false));
-    };
+  (dispatch) => {
+    const { oldPassword, newPassword } = values;
+    privateInstance
+      .patch("/api/v1/user/changepassword", {
+        oldPassword: oldPassword.trim(),
+        newPassword: newPassword.trim(),
+      })
+      .then((data) => {
+        handleClose();
+        dispatch(logOut({ toggleBackdrop, navigate, popupMsg: false }));
+        toast.success(data.data?.message);
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => setSubmitting(false));
+  };
 
 export const verifyEmail =
   ({ otp, navigate }) =>
-    (dispatch) => {
-      dispatch(setStatus(STATUS.LOADING));
-      privateInstance
-        .post("/api/v1/mail/verifyemailverificationotp", { otp })
-        .then((data) => {
-          toast.success(data.data?.message);
-          dispatch(setUser({ verified: true }));
-          navigate(-1);
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => dispatch(setStatus(STATUS.IDLE)));
-    };
+  (dispatch) => {
+    dispatch(setStatus(STATUS.LOADING));
+    privateInstance
+      .post("/api/v1/mail/verifyemailverificationotp", { otp })
+      .then((data) => {
+        toast.success(data.data?.message);
+        dispatch(setUser({ verified: true }));
+        navigate(-1);
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => dispatch(setStatus(STATUS.IDLE)));
+  };
 
 export const sendEmail = (navigate, cb) => (dispatch) => {
   dispatch(setStatus(STATUS.LOADING));
@@ -293,21 +299,21 @@ export const sendEmail = (navigate, cb) => (dispatch) => {
 
 export const deactivateAccount =
   ({ navigate, handleClose, toggleBackdrop }) =>
-    (dispatch) => {
-      dispatch(setStatus(STATUS.LOADING));
-      privateInstance
-        .patch("/api/v1/user/deactivate", {})
-        .then((data) => {
-          handleClose();
-          dispatch(logOut({ toggleBackdrop, navigate, popupMsg: false }));
-          toast.success(data?.data?.message);
-        })
-        .catch((err) => {
-          const message = err?.response?.data?.message;
-          toast.error(message || "Something went wrong.");
-        })
-        .finally(() => dispatch(setStatus(STATUS.IDLE)));
-    };
+  (dispatch) => {
+    dispatch(setStatus(STATUS.LOADING));
+    privateInstance
+      .patch("/api/v1/user/deactivate", {})
+      .then((data) => {
+        handleClose();
+        dispatch(logOut({ toggleBackdrop, navigate, popupMsg: false }));
+        toast.success(data?.data?.message);
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.message;
+        toast.error(message || "Something went wrong.");
+      })
+      .finally(() => dispatch(setStatus(STATUS.IDLE)));
+  };
 
 export const deleteAccount = (handleClose, toggleBackdrop) => (dispatch) => {
   dispatch(setStatus(STATUS.LOADING));
